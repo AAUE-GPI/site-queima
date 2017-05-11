4cc6b01973c73076714a497d003d880fd370daff
Galvão <joaoandre.galvao@gmail.com>
2017-05-11 19:42:24 +0100

    footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,13 +1,30 @@
 <footer>
-    <p class="footer-text">
-        Copyright &copy; 2017 AAUE. Todos os direitos reservados.
-    </p>
+    	<div class="row" style="width: 110%; height: 100px; background-color:black;">
+   	 		<div class="col-xs-2">
+        		<img style="margin-left: 80px; margin-top:20px; height: 60px; width: auto; float: left" src="<?php echo base_url()?>assets/logoaaue.svg">
+    		</div>
+    		<div class="col-xs-7 hidden-xs" style="padding-top:3%; padding-left:20%;">
+    		<p>
+        		AAUE Queima das Fitas 2017
+    		</p>
+    		</div>
+
+    		<div class="col-xs-3 hidden-xs" style="padding-top:1.5%" >
+    			
+    				<font size="2">
+        				SEGUE-NOS:<br>
+    				</font>
+    				<a href="https://www.facebook.com/AAUEvora/?ref=br_rs">
+    				<img style="margin-left: 0px; margin-top:5px; height: 30px; width: auto; float: left" src="<?php echo base_url()?>assets/facebook.svg" >
+    				<a href="https://www.instagram.com/aauevora/">
+    				<img style="margin-left: 20px; margin-top:5px; height: 30px; width: auto; float: left" src="<?php echo base_url()?>assets/insta.svg">
+    		</div>
+
+		</div>
 </footer>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
-
-</body>
 </html>
