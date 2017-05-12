a082b3539b913145cffb54832e9ac4afc1dfdfcc
Galvão <joaoandre.galvao@gmail.com>
2017-05-12 02:27:20 +0100

    footer completo

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,30 +1,29 @@
 <footer>
-    	<div class="row" style="margin: 0; width: 100%; height: 100px; background-color:black;">
-   	 		<div class="col col-sm-3 col-xs-6">
-        		<img style="margin-left: 80px; margin-top:20px; height: 60px; width: auto; float: left" src="<?php echo base_url()?>assets/logoaaue.svg">
-    		</div>
-    		<div class="col col-sm-6 hidden-xs" style=" margin-top: 20px;text-align: center;">
-    		<p>
-        		Associação Acdémica da Universidade de Évora
-    		</p>
-            <p>
-                Queima das Fitas 2017
-            </p>
-    		</div>
-
-    		<div class="col col-sm-3 col-xs-6" style="margin-top:20px; padding-right: 70px" >
-    			<div style="float: right; position: relative;">
-    				<font size="2">
-        				SEGUE-NOS:<br>
-    				</font>
-    				<a href="https://www.facebook.com/AAUEvora/?ref=br_rs">
-    				<img style="margin-left: 0px; margin-top:5px; height: 30px; width: auto;" src="<?php echo base_url()?>assets/facebook.svg" >
-    				<a href="https://www.instagram.com/aauevora/">
-    				<img style="margin-left: 20px; margin-top:5px; height: 30px; width: auto;" src="<?php echo base_url()?>assets/insta.svg">
-                </div>
-    		</div>
-
+	<div class="row" style="margin: 0; width: 100%; height: 100px; background-color:black;">
+		<div class="col col-sm-3 col-xs-6">
+			<img style="margin-left: 80px; margin-top:20px; height: 60px; width: auto; float: left" src="<?php echo base_url()?>assets/logoaaue.svg">
 		</div>
+		<div class="col col-sm-6 hidden-xs" style=" margin-top: 20px;text-align: center;">
+		<p>
+			Associação Acdémica da Universidade de Évora
+		</p>
+		<p>
+			Queima das Fitas 2017
+		</p>
+		</div>
+
+		<div class="col col-sm-3 col-xs-6" style="margin-top:20px; padding-right: 70px" >
+			<div style="float: right; position: relative;">
+				<font size="2">
+					SEGUE-NOS:<br>
+				</font>
+				<a href="https://www.facebook.com/AAUEvora/?ref=br_rs">
+				<img style="margin-left: 0px; margin-top:5px; height: 30px; width: auto;" src="<?php echo base_url()?>assets/facebook.svg" >
+				<a href="https://www.instagram.com/aauevora/">
+				<img style="margin-left: 20px; margin-top:5px; height: 30px; width: auto;" src="<?php echo base_url()?>assets/insta.svg">
+			</div>
+		</div>
+	</div>
 </footer>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
