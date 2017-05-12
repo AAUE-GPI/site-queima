525d50ecdf1b6472e1280857ac3df479449a5ba6
NunoLopes96 <nun0lopes@hotmail.com>
2017-05-12 01:42:45 +0100

    bug fix

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -41,7 +41,7 @@ $this->load->helper('url');
     html, body { height: 100%; width: 100%; margin: 0; }
 </style>
 
-<body style="margin: 0px; color: white; background-image: url('<?php echo base_url()?>assets/fundo_site_queima.svg') ; font-family: Arial; background-size: 100% auto">
+<body style="color: white; background-repeat: repeat-x; background-image: url('<?php echo base_url()?>assets/fundo_site_queima.svg') ; font-family: Arial; background-size: auto 100%">
 <div style="width: 100%; height: 100px; background-color:rgba(0,0,0,0.3);">
     <div>
 
